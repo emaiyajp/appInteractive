d7ae8f0acd9a48e62ea7de8de26bc17567ecbf78
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-23 09:32:33 -0600

    Initial Commit

---
+++ appInteractive/appInteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  appInteractiveApp.swift
+//  appInteractive
+//
+//  Created by Scholar on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct appInteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
